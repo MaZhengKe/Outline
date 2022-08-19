a5cfc1c1809d821cb3cceb0234492a8c9a27f601
MK <ma_zhengke@sina.com>
2022-08-19 16:09:46 +0800

    Initial commit

---
+++ Assets/Runtime/Scripts/Outline.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KuanMi
+{
+    public class Outline : MonoBehaviour
+    {
+        [SerializeField] private bool show;
+
+        private Renderer _renderer;
+        private Renderer[] _rendererList;
+
+        public OutlineFeature.RenderLayer renderLayer;
+
+        public bool containSub;
+
+        public void Show()
+        {
+            show = true;
+            SetLayer();
+        }
+
+        public void Hide()
+        {
+            show = false;
+            SetLayer();
+        }
+
+        private void OnValidate()
+        {
+            SetLayer();
+        }
+
+        private void SetLayer()
+        {
+            if (!containSub)
+            {
+                if (_renderer == null)
+                    _renderer = GetComponent<Renderer>();
+                SetLayer(_renderer);
+            }
+            else
+            {
+                if (_rendererList == null)
+                    _rendererList = GetComponentsInChildren<Renderer>();
+                foreach (var renderer1 in _rendererList)
+                {
+                    SetLayer(renderer1);
+                }
+            }
+        }
+
+        private void SetLayer(Renderer renderer)
+        {
+            if (show)
+                renderer.renderingLayerMask |= (uint)renderLayer;
+            else
+                renderer.renderingLayerMask &= ~(uint)renderLayer;
+        }
+    }
+}
---
+++ Assets/Runtime/Scripts/OutlineFeature.cs
@@ -0,0 +1,117 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace KuanMi
+{
+    public class OutlineFeature : ScriptableRendererFeature
+    {
+        [System.Flags]
+        public enum RenderLayer
+        {
+            Outline01 = (1 << 20),
+            Outline02 = (1 << 21),
+            Outline03 = (1 << 22),
+            Outline04 = (1 << 23),
+        }
+
+        public enum OutlineProfileId
+        {
+            Mask,
+            FullScreen,
+            Outline
+        }
+
+        public void CleanOutLayer()
+        {
+            var renderers = FindObjectsOfType<Renderer>();
+
+            foreach (var renderer in renderers)
+            {
+                renderer.renderingLayerMask &= ~renderingLayerMask;
+            }
+            // var outlines = FindObjectsOfType<Outline>();
+            // foreach (var outline in outlines)
+            // {
+            //     outline.SetLayer();
+            // }
+        }
+
+        [Tooltip("遮挡显示")] public bool show;
+        [Range(0f, 5f), Tooltip("轮廓宽度")] public float width = 1;
+        [Range(0, 3), Tooltip("采样次数")] public int samplePrecision = 2;
+
+        [ColorUsage(true, true), Tooltip("轮廓颜色")]
+        public Color color = new(1, 1, 1);
+
+        [SerializeField] private LayerMask layerMask;
+        [SerializeField, HideInInspector] private Shader m_Shader;
+
+        public RenderLayer renderLayer;
+
+        private uint renderingLayerMask;
+        private const string k_ShaderName = "KuanMi/Outline";
+        private Material m_Material;
+        private OutlinePass _outlinePass;
+
+        public override void Create()
+        {
+            renderingLayerMask = (uint)renderLayer;
+
+            _outlinePass = new OutlinePass(RenderQueueRange.all, layerMask, renderingLayerMask, this)
+            {
+                renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing
+            };
+        }
+
+        // Here you can inject one or multiple render passes in the renderer.
+        // This method is called when setting up the renderer once per-camera.
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            if (!GetMaterial())
+            {
+                Debug.LogErrorFormat(
+                    "{0}.AddRenderPasses(): Missing material. {1} render pass will not be added. Check for missing reference in the renderer resources.",
+                    GetType().Name, name);
+                return;
+            }
+
+            bool shouldAdd = _outlinePass.Setup(renderer, m_Material);
+            if (shouldAdd)
+            {
+                renderer.EnqueuePass(_outlinePass);
+            }
+        }
+
+        private bool GetMaterial()
+        {
+            if (m_Material != null)
+            {
+                return true;
+            }
+
+            if (m_Shader == null)
+            {
+                m_Shader = Shader.Find(k_ShaderName);
+                if (m_Shader == null)
+                {
+                    return false;
+                }
+            }
+
+            m_Material = CoreUtils.CreateEngineMaterial(m_Shader);
+
+            return m_Material != null;
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            _outlinePass?.Dispose();
+            _outlinePass = null;
+
+            CoreUtils.Destroy(m_Material);
+        }
+    }
+}
